7d8e56855f4b6e29484f7812ddd9e20055e9890a
Tom Johnson V <tomv.johnson2100@gmail.com>
2020-06-05 14:51:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,13 +4,9 @@ import OpticFlowFinal
 app = Flask(__name__)
 
 
-def returnSpeed():
-    #Put your speed function here
-    
-    return OpticFlowFinal.valRet()
 @app.route('/',methods= ['GET'])
 def aji():
-    speed=returnSpeed()
+    speed=OpticFlowFinal.valRet()
     if returnSpeed()[3] == 1:
     	return render_template('speed.html',speed=returnSpeed()[0],currVol=returnSpeed()[1],predictedVol=returnSpeed()[2])
     else:
